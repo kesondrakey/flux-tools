af1b6c70ef5aa3b5b02f7ad5a9d50be2292f95df
kesondrakey <kesondra.key@gmail.com>
2025-06-11 13:44:23 -0400

    Small updates to Vignette and library call

--- R/zzz.R
+++ R/zzz.R
@@ -5,10 +5,11 @@
     "fluxtools: Interactive QA/QC for AmeriFlux Data\n\n",
     "To run the app, replace with your site’s UTC offset (i.e., –5 for EST):\n",
     "fluxtools::run_flux_qaqc(-5)\n\n",
+    "To see the how-to vignette, run:\n\n",
+    "vignette('introduction', package = 'fluxtools')\n\n",
     "Citation:\n",
     "  Key, K. (2025). fluxtools (version 0.2.0) [Computer software]. Zenodo. ",
     "https://doi.org/10.5281/zenodo.15597159\n"
   )
 }
-
 
