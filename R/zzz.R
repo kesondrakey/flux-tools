d3224056196c74719c0e2ba662bcb9ed8d25e045
kesondrakey <kesondra.key@gmail.com>
2025-06-11 13:19:59 -0400

    updating

--- R/zzz.R
+++ R/zzz.R
@@ -3,9 +3,11 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage(
     "fluxtools: Interactive QA/QC for AmeriFlux Data\n",
+    "To run the app, simply replace with your site’s UTC offset (e.g., –5 for EST):\n",
+    "fluxtools::run_flux_qaqc(-5)\n\n",
     "Citation:\n",
-    "  Key, K. (2025). fluxtools (version 0.1.1) [Computer software]. Zenodo. ",
-    "https://doi.org/10.5281/zenodo.15597309\n"
+    "  Key, K. (2025). fluxtools (version 0.2.0) [Computer software]. Zenodo. ",
+    "https://doi.org/10.5281/zenodo.15597159\n"
   )
 }
 
