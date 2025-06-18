633ab60e909bb9df6fde2ca54609a06b48aee7da
kesondrakey <kesondra.key@gmail.com>
2025-06-17 20:45:22 -0400

    minor updates

--- R/zzz.R
+++ R/zzz.R
@@ -1,18 +1,9 @@
 # R/zzz.R
 
-# .onAttach <- function(libname, pkgname) {
-#   packageStartupMessage(
-#     "fluxtools: Interactive QA/QC for AmeriFlux Data\n\n",
-#     "Citation:\n",
-#     "  Key, K. (2025). fluxtools (version 0.2.0) [Computer software]. Zenodo. ",
-#     "https://doi.org/10.5281/zenodo.15597159\n"
-#   )
-# }
-
-
 .onAttach <- function(libname, pkgname) {
   version <- utils::packageVersion(pkgname)
-  packageStartupMessage("Welcome to fluxtools!")
+  packageStartupMessage("Welcome to fluxtools!\n")
   packageStartupMessage("Version: ", version)
   packageStartupMessage("To cite, run citation('fluxtools')")
+  packageStartupMessage("To see the vignette, run vignette('introduction', package = 'fluxtools')")
 }
